8f1bf0ef5dcbd4c6a1cb41d8a4395a23dc22e5d6
SirCesarium <cesarium3600@gmail.com>
2026-03-26 15:26:53 -0400

    feat: initial branding and cli skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use console::{Emoji, style};
+
+const ASCII: &str = r#"
+                            __ 
+   ______      _____  ___  / /_
+  / ___/ | /| / / _ \/ _ \/ __/
+ /__  /| |/ |/ /  __/  __/ /_  
+/____/ |__/|__/\___/\___/\__/  "#;
+
 fn main() {
-    println!("Hello, world!");
+    let logo = style(ASCII).magenta().bold();
+    let candy = Emoji("🍬 ", "!");
+
+    println!("{}{}", logo, candy);
+
+    println!(
+        "\n{}",
+        style("— A blazing-fast code health analyzer :)")
+            .italic()
+            .cyan()
+    );
+    println!("{}", style("─".repeat(45)).dim());
 }
