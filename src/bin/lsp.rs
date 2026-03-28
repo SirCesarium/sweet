c718f93d6f71ba1f4cdcd513743a6a00b3f6e4de
SirCesarium <cesarium3600@gmail.com>
2026-03-28 15:59:36 -0400

    feat: add optional LSP binary and set swt as default-run

---
+++ src/bin/lsp.rs
@@ -0,0 +1,45 @@
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::{
+    InitializeParams, InitializeResult, InitializedParams, MessageType, ServerCapabilities,
+    TextDocumentSyncCapability, TextDocumentSyncKind,
+};
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "Sweet LSP server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
